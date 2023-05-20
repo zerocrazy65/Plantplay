91535b714cd0fb6ac3e8063fa96955f5a02ee8e1
comnows <watthanandh.l@mail.kmutt.ac.th>
2023-05-21 00:15:34 +0700

    ADD Splash screen and icon

---
+++ lib/screens/start/splash.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/screens/authentication/login.dart';
+import 'package:flutter_application_1/theme/style.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  // late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(Duration(seconds: 2), (){
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        ),
+      );
+    });
+    // _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // _controller.dispose();
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    //     overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: ColorTheme.introPageColor),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/logo/PlantPlay_Splash.png", width: 100),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Plant'.toUpperCase(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: ColorTheme.whiteColor,
+                      fontSize: 38,
+                    ),
+                  ),
+                  Text(
+                    'Play'.toUpperCase(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: ColorTheme.highlightColor,
+                      fontSize: 38,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+      ),
+    );
+  }
+}
