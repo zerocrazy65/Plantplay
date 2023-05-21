1a5541632b7a585cda0ee31d34792c529c2af006
zerocrazy65 <katapat70@gmail.com>
2023-05-21 10:28:08 +0700

    delete comment & update version

--- lib/screens/start/splash.dart
+++ lib/screens/start/splash.dart
@@ -12,28 +12,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  // late AnimationController _controller;
-
   @override
   void initState() {
     super.initState();
-    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
-    Future.delayed(const Duration(seconds: 2), (){
+    Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (_) => const LoginPage(),
         ),
       );
     });
-    // _controller = AnimationController(vsync: this);
   }
 
   @override
   void dispose() {
-    // _controller.dispose();
-    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-    //     overlays: SystemUiOverlay.values);
     super.dispose();
   }
 
@@ -42,36 +34,33 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       body: Container(
         width: double.infinity,
-        decoration: const BoxDecoration(
-          color: ColorTheme.introPageColor),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/logo/PlantPlay_Splash.png", width: 100),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Plant'.toUpperCase(),
+        decoration: const BoxDecoration(color: ColorTheme.introPageColor),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/logo/PlantPlay_Splash.png", width: 100),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Plant'.toUpperCase(),
                     style: GoogleFonts.quicksand(
                       fontWeight: FontWeight.bold,
                       color: ColorTheme.whiteColor,
                       fontSize: 38,
-                    )
+                    )),
+                Text(
+                  'Play'.toUpperCase(),
+                  style: GoogleFonts.quicksand(
+                    fontWeight: FontWeight.bold,
+                    color: ColorTheme.highlightColor,
+                    fontSize: 38,
                   ),
-                  Text(
-                    'Play'.toUpperCase(),
-                    style: GoogleFonts.quicksand(
-                      fontWeight: FontWeight.bold,
-                      color: ColorTheme.highlightColor,
-                      fontSize: 38,
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
