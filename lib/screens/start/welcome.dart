1c5130dc6c6ba9f9e1b8d572cdc028989f69b8d4
zerocrazy65 <katapat70@gmail.com>
2023-05-22 23:23:51 +0700

    Update startpage pic&fix

--- lib/screens/start/welcome.dart
+++ lib/screens/start/welcome.dart
@@ -11,76 +11,99 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  late AssetImage _backgroundImage;
+  bool _isImageLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _backgroundImage = const AssetImage('assets/img/plant_bg.jpg');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    await precacheImage(_backgroundImage, context);
+    setState(() {
+      _isImageLoaded = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/logo/PlantPlay_noText.png"),
-            fit: BoxFit.cover,
-            ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 100),
-            Text(
-              'Welcome',
-              style: GoogleFonts.quicksand(
-                fontSize: 64,
-                fontWeight: FontWeight.w600,
-                color: ColorTheme.introPageColor
+      body: _isImageLoaded
+          ? Container(
+              padding: const EdgeInsets.all(15),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/img/plant_bg.jpg"),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const SizedBox(height: 45),
-            Text(
-              'we\'re glad that\nyou are here',
-              style: GoogleFonts.quicksand(
-                fontSize: 24,
-                fontWeight: FontWeight.w400,
-                color: ColorTheme.introPageColor
-              ),
-            ),
-            const SizedBox(height: 250),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (_) => const LoginPage())
-                    );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color(0xD947734D)),
-                    minimumSize: MaterialStateProperty.all<Size>(Size(250, 60)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))
-                    )
-                  ),
-                  child: Center(child: 
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 30),
                     Text(
-                      'Lets get started',
+                      'Welcome',
                       style: GoogleFonts.quicksand(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                        color: ColorTheme.whiteColor
+                          fontSize: 64,
+                          fontWeight: FontWeight.w600,
+                          color: ColorTheme.introPageColor),
+                    ),
+                    const SizedBox(height: 40),
+                    Text(
+                      'we\'re glad that\nyou are here',
+                      style: GoogleFonts.quicksand(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w400,
+                          color: ColorTheme.introPageColor),
+                    ),
+                    // const SizedBox(height: 250),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (_) => const LoginPage()));
+                              },
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Color(0xD947734D)),
+                                  minimumSize: MaterialStateProperty.all<Size>(
+                                      Size(250, 60)),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10)))),
+                              child: Text(
+                                'Lets get started',
+                                style: GoogleFonts.quicksand(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600,
+                                    color: ColorTheme.whiteColor),
+                              )),
+                        ],
                       ),
                     ),
-                  )
-                ),
-              ],
+                    const SizedBox(height: 100),
+                  ]),
             )
-            
-          ],
-        ),
-      ),
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
